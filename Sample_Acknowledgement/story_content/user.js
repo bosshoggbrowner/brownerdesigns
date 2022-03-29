e88b0c1f961dd003630d544f0d42b250ba042a2c
bosshoggbrowner <59402321+bosshoggbrowner@users.noreply.github.com>
2022-03-29 10:48:16 -0400

    Add files via upload

---
+++ Sample_Acknowledgement/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5zhEIG2xOvG":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.open('story_content/external_files/Sample Compliance Policy.pdf', "_blank", 'toolbar=0,location=0,menubar=0,height=700,width=1000');
+}
+
